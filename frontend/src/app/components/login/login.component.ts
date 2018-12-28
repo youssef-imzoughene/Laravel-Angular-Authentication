49634e3e555b4b8f90d822328bb9ec713133f0a7
imzoughene <imzoughene.youssef.01@gmail.com>
2018-12-28 12:13:51 +0100

    create login form http request

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
+import { HttpClient, HttpHeaders,HttpErrorResponse } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  public form =  {
+    email:null,
+    password:null
+  }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+   
   }
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  
+    const httpOptions = {
+			headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+		};
+  
+    let Form = JSON.stringify(f.value);
+    return this.http.post("http://localhost:8000/api/auth/login", Form, httpOptions).subscribe(
+      tap((data) => {
+        console.log(data)
+      })
+     );
+   
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			console.error(error);
+			console.log(`${operation} failed: ${error.message}`);
+
+			return of(result as T);
+		};
+	}
 
 }
